52ad05ab7f4dbc3c8cfe0afada2803e388f8a024
jeevan <jk29102023@gmail.com>
2026-02-19 22:15:27 +0530

    how to hash passwoirds

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
-import { error } from "console";
+import bcrypt from "bcryptjs";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -10,9 +10,9 @@ const createUser = asyncHandler(async (req, res) => {
 
   const userExits = await User.findOne({ email });
   if (userExits) res.status(400).send("user exits already");
-
-  const newUser = new User({ username, email, password });
-  console.log(newUser);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({
